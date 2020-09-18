0e6de0ec451c954097b4717e773e445a36b9a4f5
AlexLoner <lukyanov9567@gmail.com>
2020-09-18 14:00:22 +0300

    lection2

---
+++ Lecture_2/modules_examples/test_module2.py
@@ -0,0 +1,11 @@
+
+def some_cool_function():
+    print('I do nothing')
+    
+def func(a: str, b: str):
+    return f"{a.capitalize()} {b.upper()}"
+
+def prod(a=1, *args):
+    for i in args:
+        a *= i
+    return a
